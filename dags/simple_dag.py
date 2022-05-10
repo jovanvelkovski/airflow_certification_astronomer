df4a4077a6293b92dc1289826369cc15510a2fae
jovanvelkovski <velkovski.jovan@yahoo.com>
2022-05-10 11:26:33 +0200

    bash operator and defining dependencies

--- dags/simple_dag.py
+++ dags/simple_dag.py
@@ -1,8 +1,10 @@
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
 from airflow.sensors.filesystem import FileSensor
 from airflow.utils.dates import days_ago
+from airflow.models.baseoperator import chain, cross_downstream
 from datetime import datetime, timedelta
 
 default_args = {
@@ -22,6 +24,10 @@ default_args = {
 def _downloading_data(**kwargs):
     with open('/tmp/my_file.txt', 'w') as f:
         f.write('my_data')
+
+
+def _checking_data():
+    print('checking data')
 
 
 with DAG(
@@ -44,9 +50,32 @@ with DAG(
         # op_kwargs={'my_param': 42} #passing our own parameters to func _downloading_data
     )
 
+    checking_data = PythonOperator(
+        task_id='checking_data',
+        python_callable=_checking_data
+    )
+
     waiting_for_data = FileSensor(
         task_id='waiting_for_data',
         fs_conn_id='fs_default', #create it on the ui
         filepath='my_file.txt'
         # ,poke_interval=30 #how often should the sensor check for the file
     )
+
+    processing_data = BashOperator(
+        task_id='processing_data',
+        bash_command='exit 0'
+    )
+
+    # downloading_data.set_downstream(waiting_for_data)
+    # waiting_for_data.set_downstream(processing_data)
+
+    # processing_data.set_upstream(waiting_for_data)
+    # waiting_for_data.set_upstream(downloading_data)
+
+    # downloading_data >> waiting_for_data >> processing_data
+
+    # downloading_data >> [waiting_for_data >> processing_data]
+
+    chain(downloading_data, waiting_for_data, checking_data, processing_data)
+    # cross_downstream([downloading_data, checking_data], [waiting_for_data, processing_data])
